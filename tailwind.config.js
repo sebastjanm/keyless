90c6e10980af5683a0c91d13014fe8cd2d64f495
Sebastjan Mislej <sebastjan@lecturehub.com>
2024-07-23 10:23:04 +0200

    project updated

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.html', // Adjusted to match the HTML files in your public directory
+    './public/**/*.js', // Adjusted to match the JavaScript files in your public directory
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#0067b6', // Custom color for your footer
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    // other plugins if needed
+  ],
+}
